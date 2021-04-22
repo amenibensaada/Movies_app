d69d215e7ab4d44dfcedb309a01c71bc16a89250
amenibensaada <amenisaada19@gmail.com>
2021-04-22 01:11:50 +0100

    creating movie details page

--- src/pages/MoviesListPage.jsx
+++ src/pages/MoviesListPage.jsx
@@ -7,7 +7,6 @@ import { Link } from "react-router-dom";
 export default class MoviesListPage extends Component {
   constructor() {
     super();
-
     this.state = {
       movies: [],
       searchField: "",
